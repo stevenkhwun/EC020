6451d09f62d83202089ee824db45a7e05b96fbf0
stevenkhwun <stevenkhwun@gmail.com>
2023-06-25 09:21:56 +0800

    File modified.

--- 03_Heteroskedasticity_Script.R
+++ 03_Heteroskedasticity_Script.R
@@ -1,3 +1,6 @@
 library(wooldridge)
 data("wage2")
 wage2_ols1 <- lm(lwage ~ educ + + exper, data=wage2)
+wage2$ols1resid <- resid(wage2_ols1)            # get residuals
+wage2$fitted1wage <- fitted.values(wage2_ols1)  # get fitted values
+wage2$ols1residsq <- wage2$ols1resid^2          # square the residuals
